87508372f7b46d3b9bb3008d9af75da094b8f1cc
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-01-09 03:51:57 -0400

    Added Andrew as collaborator and added Unit Tests.

--- tests/tests.js
+++ tests/tests.js
@@ -1,14 +1,42 @@
 // Dependencies
 var assert = require("assert")
 var selfService = require("../lib/");
+var creds;
+try {
+    creds = require('../credentials.json');
+} catch (e) {
+    console.log(e);
+    console.log("Please create a file `credentials.json` in the project root directory.");
+    console.log("{ 'username': 'A12345', 'password': 'password' }");
+    process.exit(1);
+}
+
+var s = new selfService;
 
 // Run tests
 describe('User', function() {
-  describe('#save()', function(){
+  describe('#Login()', function(){
     
-    it('should save without error', function(done){
+    it('should login without error', function(done){
       
-      selfService(done);
+        s.login({'username': creds.username, 'password': creds.password }, function(error, response, localService) {
+            //console.log("Completed!");
+            //console.log(error, response, body);
+            //console.log(error);
+
+            done();
+
+            describe('#weekAtAGlance()', function() {
+                it('should get Week at a Glance without error', function(done){
+
+                    localService.weekAtAGlance({ /*'startDate': new Date()*/ }, function(error, response, courses) {
+                        //console.log("Completed!", courses);
+                        done();
+                    });
+                });
+            });
+
+        });
 
     })
 
