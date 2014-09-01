e49829fa465a8afa03672cc00e8895f0e78cfb17
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-09-01 17:52:11 -0300

    Closes #4. Return error in Login callback on failed Login authentication

--- tests/tests.js
+++ tests/tests.js
@@ -6,49 +6,81 @@ try {
     creds = require('../credentials.json');
 } catch (e) {
     console.log(e);
-    console.log("Please create a file `credentials.json` in the project root directory.");
+    console.log(
+        "Please create a file `credentials.json` in the project root directory."
+    );
     console.log("{ 'username': 'A12345', 'password': 'password' }");
     process.exit(1);
 }
 
-var s = new selfService();
-
 // Run tests
 describe('User', function() {
-  describe('#Login()', function(){
+    describe('#Login()', function() {
 
-    it('should login without error', function(done){
+        it('should login with error', function(done) {
+            var s = new selfService();
 
-        s.login({'username': creds.username, 'password': creds.password }, function(error, response, localService) {
-            //console.log("Completed!");
-            //console.log(error,  response.statusCode, localService);
-            //console.log(error);
+            s.login({
+                'username': "A123456789",
+                'password': "password"
+            }, function(error, response, localService) {
+                // console.log("Completed!", error);
+                // console.log(response,
+                //     localService);
 
-            if (!error) {
-                done();
-            } else {
-                throw error;
-            }
-
-            describe('#weekAtAGlance()', function() {
-                it('should get Week at a Glance without error', function(done){
-
-                    this.timeout(10000);
-
-                    localService.weekAtAGlance({ /*'startDate': new Date()*/ }, function(error, response, courses) {
-
-                        console.log("Completed!", courses);
-
-                        if (!error) {
-                            done();
-                        } else {
-                            throw error;
-                        }
-                    });
-                });
+                if (error) {
+                    done();
+                } else {
+                    throw new Error(
+                        "Expected an error for invalid login credentials."
+                    );
+                }
             });
+        });
 
-            /*
+        it('should login without error', function(done) {
+            var s = new selfService();
+
+            s.login({
+                'username': creds.username,
+                'password': creds.password
+            }, function(error, response, localService) {
+                //console.log("Completed!");
+                //console.log(error,  response.statusCode, localService);
+                //console.log(error);
+
+                if (!error) {
+                    done();
+                } else {
+                    throw error;
+                }
+
+                describe('#weekAtAGlance()', function() {
+                    it(
+                        'should get Week at a Glance without error',
+                        function(done) {
+
+                            this.timeout(10000);
+
+                            localService.weekAtAGlance({
+                                'startDate': new Date()
+                            }, function(error, response,
+                                courses) {
+
+                                // console.log(
+                                //     "Completed!",
+                                //     courses);
+
+                                if (!error) {
+                                    done();
+                                } else {
+                                    throw error;
+                                }
+                            });
+                        });
+                });
+
+                /*
             describe("#course()", function() {
                 it('should get a Course without error', function(done) {
                     this.timeout(5000);
@@ -65,9 +97,9 @@ describe('User', function() {
             });
             */
 
+            });
+
         });
 
     });
-
-  });
-});
+})
